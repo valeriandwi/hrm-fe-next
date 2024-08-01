76758218abd348449807a937c5cc800198675c8b
Valerian <valerian.purnomo@inticorp.tech>
2024-08-01 16:28:11 +0700

    Add dummy data for job list

--- src/features/job-list/dummyData.tsx
+++ src/features/job-list/dummyData.tsx
@@ -1,8 +1,62 @@
 export const dummyData = [
   {
-    role: "Frontend Web",
+    role: "Frontend Web Developer",
     experiences: "7 years",
     location: "Jakarta, Indonesia",
     lastUpdated: "10-02-2024",
+  },
+  {
+    role: "Backend Developer",
+    experiences: "5 years",
+    location: "New York, USA",
+    lastUpdated: "15-03-2024",
+  },
+  {
+    role: "Full Stack Developer",
+    experiences: "6 years",
+    location: "London, UK",
+    lastUpdated: "12-01-2024",
+  },
+  {
+    role: "Mobile App Developer",
+    experiences: "4 years",
+    location: "Sydney, Australia",
+    lastUpdated: "22-04-2024",
+  },
+  {
+    role: "DevOps Engineer",
+    experiences: "8 years",
+    location: "Berlin, Germany",
+    lastUpdated: "30-05-2024",
+  },
+  {
+    role: "Data Scientist",
+    experiences: "3 years",
+    location: "Toronto, Canada",
+    lastUpdated: "18-06-2024",
+  },
+  {
+    role: "Machine Learning Engineer",
+    experiences: "2 years",
+    location: "San Francisco, USA",
+    lastUpdated: "25-07-2024",
+  },
+  {
+    role: "UI/UX Designer",
+    experiences: "5 years",
+    location: "Paris, France",
+    lastUpdated: "05-08-2024",
+  },
+  {
+    role: "Cybersecurity Analyst",
+    experiences: "7 years",
+    location: "Tokyo, Japan",
+    lastUpdated: "11-09-2024",
+  },
+  {
+    role: "Cloud Architect",
+    experiences: "9 years",
+    location: "Singapore",
+    lastUpdated: "29-10-2024",
   },
 ];
